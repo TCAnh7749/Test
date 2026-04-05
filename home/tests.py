d6a4bb090d39f183dbd2f941b492c8d56d8cc93f
TCAnh <112803260+TCAnh7749@users.noreply.github.com>
2026-04-05 15:32:22 +0700

    Change SimpleTestCase to TestCase in tests.py

--- home/tests.py
+++ home/tests.py
@@ -1,8 +1,6 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import TestCase
 
-class SimpleTest(SimpleTestCase):
+class SimpleTest(TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
-# Create your tests here.
